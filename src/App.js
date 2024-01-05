0772f031bab555813a5faca1128b81ca8acdbc7b
Abhishek_kumar <Abhishku611@gmail.com>
2024-01-05 20:08:46 +0530

    making form component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
+import Form from './Form';
 
 function App() {
 
@@ -14,51 +15,13 @@ function App() {
   //   alert(`Weight: ${weight} KGs\nHeight: ${height} cms`);
     
   // }
-  const [formData, setFormData] = useState({});
-  const [formValues, setFormValues] = useState([]);
+  
 
   // form submission handler
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setFormValues(Object.entries(formData));
-  };
-
-  // input change handler
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+  
   return (
-    <div className="App d-flex justify-content-center align-items-center">
-      <div className='my-3 form-Background w-50' >
-        <form onSubmit={handleSubmit}>
-          <label for='weight' className='my-3 mx-3'> Weight : </label>
-          <input type='number' placeholder='in KGs' name='weight' onChange={handleChange} />
-
-          <br/>
-          <label for='height' className='my-3 mx-3'> Height : </label>
-          <input type='number' placeholder='in cms' name='height' onChange={handleChange}/>
-          <br/>
-
-          <button type='submit' className=' my-3 btn btn-warning' >
-          Submit!
-        </button>
-        </form>
-        <div>
-        {/* <pre>{JSON.stringify(formValues, null, 2)}</pre> */}
-        <div>
-        <h2>Form Values:</h2>
-        <ul>
-          {formValues.map(([key, value]) => (
-            <li key={key}>
-              <strong>{key}:</strong> {value}
-            </li>
-          ))}
-        </ul>
-      </div>
-      </div>
-        
-      </div>
-      
+    <div>
+      <Form />
     </div>
   );
 }
---
+++ src/Form.js
@@ -0,0 +1,56 @@
+import './Form.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+
+function Form(){
+const [formData, setFormData] = useState({});
+const [formValues, setFormValues] = useState([]);
+const [disp,setDisp] = useState(false);
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    setDisp(true);
+    setFormValues(Object.entries(formData));
+  };
+
+  // input change handler
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return(
+    <div className="App d-flex justify-content-center align-items-center">
+      <div className='my-3 form-Background w-50' >
+        <form onSubmit={handleSubmit}>
+          <label for='weight' className='my-3 mx-3'> Weight : </label>
+          <input type='number' placeholder='in KGs' name='weight' onChange={handleChange} />
+
+          <br/>
+          <label for='height' className='my-3 mx-3'> Height : </label>
+          <input type='number' placeholder='in cms' name='height' onChange={handleChange}/>
+          <br/>
+
+          <button type='submit' className=' my-3 btn btn-warning' >
+          Submit!
+        </button>
+        </form>
+        <div>
+        {/* <pre>{JSON.stringify(formValues, null, 2)}</pre> */}
+        <div>
+        {disp && <h2>Form Values: </h2>}
+        <ul>
+          {formValues.map(([key, value]) => (
+            <li key={key}>
+              <strong>{key}:</strong> {value}
+            </li>
+          ))}
+        </ul>
+      </div>
+      </div>
+        
+      </div>
+      
+    </div>
+  );
+  }
+export default Form;
